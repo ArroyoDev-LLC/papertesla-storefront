9851db0ad44eb437e7fff1f457e0b6b06f3e08af
Braden Mars <bradenmars@bradenmars.me>
2020-02-05 16:19:29 -0600

    fix(reactjs): reactjs inline setup

--- babel.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    presets: ["@babel/env", "@babel/preset-react"],
-    plugins: ["@babel/plugin-transform-runtime", "@babel/plugin-transform-strict-mode"]
-}
--- js/Review.js
+++ js/Review.js
@@ -1,65 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
- class ReviewFormButton extends React.Component 
- {
-   constructor(props)
-   {
-     super(props); 
-   }
-   handleClick()
-   {
-     alert('React is working');
-     return(render(<ReactForm/>, document.querySelector('#react-window')));
-   }
-  render(){
-   return (
-    <button onClick={this.handleClick}>Write a Review</button>
-   );
+class ReviewFormButton extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  handleClick() {
+    alert("React is working");
+    return render(<ReactForm />, document.querySelector("#react-window"));
+  }
+  render() {
+    return <button onClick={this.handleClick}>Write a Review</button>;
   }
 }
-//  <button onClick={handleClick()}>Write a Review</button>
-class ReactForm extends React.Component
-{
-  constructor(props) 
-  {
+
+class ReactForm extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = {value: ' '};
+    this.state = { value: " " };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) 
-  {
-    this.setState({value: event.target.value});
+  handleChange(event) {
+    this.setState({ value: event.target.value });
   }
-  handleSubmit(event)
-  {
-    alert('Form Submitted! Thank you for your review!' + this.state.value);
+  handleSubmit(event) {
+    alert("Form Submitted! Thank you for your review!" + this.state.value);
     event.preventDefault();
   }
-  render()
-  {
+  render() {
     return (
       <form onSubmit={this.handleSubmit}>
-      <label>
-        Name: <input type="text" value={this.state.value} />
-      </label>
-      <label>
-        Rating: <input type="text" vale={this.state.value} />
-      </label>
-      <input type="textarea" defaultValue="Write Review Here!"/>
-      <button type="submit">Submit</button>
-      </form>  
-    )
+        <label>
+          Name: <input type="text" value={this.state.value} />
+        </label>
+        <label>
+          Rating: <input type="text" vale={this.state.value} />
+        </label>
+        <input type="textarea" defaultValue="Write Review Here!" />
+        <button type="submit">Submit</button>
+      </form>
+    );
   }
 }
 
-const domContainer = document.querySelector('#react-button');
 ReactDOM.render(
   <div>
     <ReviewFormButton />
   </div>,
- domContainer
+  document.getElementById("react-button")
 );
